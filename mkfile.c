9b60b0ef88c2616e2663d04231dcbcece59da6e0
troberts97 <t.roberts@stud.unibas.ch>
2024-04-18 17:45:46 +0200

    Creates a file as well now and you only have to write tagname instead of user.tagname

--- mkfile.c
+++ mkfile.c
@@ -3,19 +3,31 @@
 #include <string.h>
 #include <stdio.h>
 
+#define MAX_ATTR_SIZE 1024
+
 int main (int argc, char * argv[]) {
     if (argc < 4) {
-        printf("Usage: %s <file_path> user.<tagname> <tagvalue>\n", argv[0]);
+        printf("Usage: %s <file_path> <tagname> <tagvalue>\n", argv[0]);
         return 1;
     }
 
-    int fd2 = open(argv[1], O_RDWR | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO);
+    // Create file with rwx permissions for user, group, other
+    int file = open(argv[1], O_RDWR | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO);
+    if (file == -1) {
+        perror("open");
+        return 1;
+    }
+
+    // Concatenate user.argv[2] to get user.<tagname>
+    char attr_name[MAX_ATTR_SIZE];
+    sprintf(attr_name, "user.%s",argv[2]);
 
     // Set the extended attribute on the file
-    if (setxattr(argv[1], argv[2], argv[3], strlen(argv[3]), 0) == -1) {
+    if (setxattr(argv[1], attr_name, argv[3], strlen(argv[3]), 0) == -1) {
         perror("setxattr");
         return 1;
     }
 
-    printf("Tag %s set successfully for file %s with name %s\n", argv[3], argv[1], argv[1]);
+    // Print statement of extended attribute
+    printf("Tag %s set successfully for file %s with name %s\n", argv[3], argv[1], attr_name);
 }
