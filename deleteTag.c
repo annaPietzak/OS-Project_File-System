0911f5c062ab6fafb6594602a32f4708fa6b5bac
troberts97 <t.roberts@stud.unibas.ch>
2024-04-18 17:47:33 +0200

    Only have to write tagname instead of user.tagname

--- deleteTag.c
+++ deleteTag.c
@@ -1,18 +1,26 @@
 #include <sys/xattr.h>
 #include <stdio.h>
 
+#define MAX_ATTR_SIZE 1024
+
 int main(int argc, char *argv[]) {
     if (argc < 3) {
-        printf("Usage: %s <file_path> user.<tagname>\n", argv[0]);
+        printf("Usage: %s <file_path> <tagname>\n", argv[0]);
         return 1;
     }
 
-    if (removexattr(argv[1], argv[2]) == -1) {
+    // Concatenate user.argv[2] to get user.<tagname>
+    char attr_name[MAX_ATTR_SIZE];
+    sprintf(attr_name, "user.%s",argv[2]);
+
+    // Remove the specified extended attribute
+    if (removexattr(argv[1], attr_name) == -1) {
         perror("removexattr");
         return 1;
     }
 
-    printf("Tag %s removed successfully from file %s\n", argv[2], argv[1]);
+    // Print when it was successful
+    printf("Tag %s removed successfully from file %s\n", attr_name, argv[1]);
 
     return 0;
 }
--- list.c
+++ list.c
@@ -5,20 +5,26 @@
 
 int main (int argc, char * argv[]) {
     if (argc < 3) {
-        printf("Usage: %s <file_path> user.<tagname>\n", argv[0]);
+        printf("Usage: %s <file_path> <tagname>\n", argv[0]);
         return 1;
     }
 
     char attr_value[MAX_ATTR_SIZE];
-    ssize_t attr_size;
 
-    // List the extended attribute
-    attr_size = getxattr(argv[1], argv[2], attr_value, sizeof(attr_value));
+    // Concatenate user.argv[2] to get user.<tagname>
+    char attr_name[MAX_ATTR_SIZE];
+    sprintf(attr_name, "user.%s",argv[2]);
+
+    // Get the extended attribute and give it a size
+    ssize_t attr_size = getxattr(argv[1], attr_name, attr_value, sizeof(attr_value));
     if (attr_size == -1) {
         perror("getxattr");
         return 1;
     }
 
+    // Null terminate the extended attribute value
+    attr_value[attr_size] = '\0';
+
     // Print the value of the extended attribute
-    printf("Tag with attribute name %s has value %s\n", argv[2], attr_value);
+    printf("Tag with attribute name %s has value %s\n", attr_name, attr_value);
 }
