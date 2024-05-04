caa809e335b37582280b974b0fdc834059d43116
troberts97 <t.roberts@stud.unibas.ch>
2024-05-04 19:22:02 +0200

    Modify the testing script slightly

--- createTest.sh
+++ createTest.sh
@@ -8,4 +8,9 @@ mkdir -p test/test2/test3
 
 # Execute mkfile.o with the given arguments
 ./mkfile.o test/test2/test3/test.txt test test
+./mkfile.o test/test2/test3/test2.txt test test2
+./mkfile.o test/test2/test3/test3.txt test test3
+./mkfile.o test/test2/test3/test4.txt test test4
+./mkfile.o test/test2/test5.txt test test5
 ./mkfile.o test/uni.txt uni uni
+./mkfile.o test/test.txt test test
