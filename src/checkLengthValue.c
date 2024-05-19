c901967a40f9eeccecb509ab62a55877431e6807
troberts97 <t.roberts@stud.unibas.ch>
2024-05-19 18:45:14 +0200

    The error print is now inside mkfile.c instead of this file

--- src/checkLengthValue.c
+++ src/checkLengthValue.c
@@ -3,8 +3,6 @@
 // Check length of tagValue
 int check_tag_value(const char * tagValue) {
     if (strlen(tagValue) >= MAX_ATTR_VALUE_SIZE) {
-        fprintf(stderr, "Error: The length of the tag_value exceeds the limit of %d characters\n",
-                MAX_ATTR_VALUE_SIZE - 1);
         return 1;
     }
     return 0;
