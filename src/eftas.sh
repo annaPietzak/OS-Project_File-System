5ac48990f30b7008713b661edbd23166bc360b6f
troberts97 <t.roberts@stud.unibas.ch>
2024-05-19 17:56:09 +0200

    Change from static user to $USER

--- src/eftas.sh
+++ src/eftas.sh
@@ -9,29 +9,29 @@ fi
 case "$1" in
   "help")
 	# Help case
-  	source /home/bobadmin/OS-Project_File-System/src/open_help.sh
+  	source /home/$USER/OS-Project_File-System/src/open_help.sh
     ;;
   "mkfile")
   	# Make a file case
-    /home/bobadmin/OS-Project_File-System/src/mkfile.o $2 $3 $4
+    /home/$USER/OS-Project_File-System/src/mkfile $2 $3 $4
     ;;
   "lsDifferent2")
     # Our version of ls case
 	  if [ -z "$2" ]; then
 	    # When there are no additional arguments, reset positional parameters
 	    set --
-	    source /home/bobadmin/OS-Project_File-System/src/lsDifferent2.sh
+	    source /home/$USER/OS-Project_File-System/src/lsDifferent2.sh
 	  else
-	    source /home/bobadmin/OS-Project_File-System/src/lsDifferent2.sh "$2"
+	    source /home/$USER/OS-Project_File-System/src/lsDifferent2.sh "$2"
 	  fi
    	;;
   "deleteTag")
     # Delete tag from file case
-	  /home/bobadmin/OS-Project_File-System/src/deleteTag.o $2 $3
+	  /home/$USER/OS-Project_File-System/src/deleteTag $2 $3
    	;;
   "listAll")
    	# List all tags from specific file case
-   	/home/bobadmin/OS-Project_File-System/src/listAll.o $2
+   	/home/$USER/OS-Project_File-System/src/listAll $2
    	;;
   *) 
     # Default case: find files and folders with the extended attribute
