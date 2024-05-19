211b4c30d4582ac9d9a8439b2774dbf5b1346a1b
troberts97 <t.roberts@stud.unibas.ch>
2024-05-19 18:44:43 +0200

    The error print is now inside mkfile.c instead of this file

--- src/checkLengthName.c
+++ src/checkLengthName.c
@@ -5,8 +5,6 @@ int check_tag_name(const char * tagName) {
     size_t fullTagNameLength = strlen("user.") + strlen(tagName);
 
     if (fullTagNameLength >= MAX_ATTR_NAME_SIZE) {
-        fprintf(stderr, "Error: The total length of 'user.%s' exceeds the limit of %d characters\n",
-                tagName, MAX_ATTR_NAME_SIZE - 1);
         return 1;
     }
     return 0;
