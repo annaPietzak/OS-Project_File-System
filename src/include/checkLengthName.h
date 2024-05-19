86d7288280c91f10db988a1c2329dff73764407d
troberts97 <t.roberts@stud.unibas.ch>
2024-05-19 17:50:45 +0200

    Add checkLengthName.c as separate code since it's used multiple times

---
+++ src/include/checkLengthName.h
@@ -0,0 +1,11 @@
+#ifndef CHECKLENGTHNAME_H
+#define CHECKLENGTHNAME_H
+
+#include <string.h>
+#include <stdio.h>
+
+#define MAX_ATTR_NAME_SIZE 255
+
+int check_tag_name(const char * tagName);
+
+#endif //CHECKLENGTHNAME_H
