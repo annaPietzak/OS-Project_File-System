7b0a1e7095788759a7702c3d66212d03cc1f438d
troberts97 <t.roberts@stud.unibas.ch>
2024-05-21 11:43:58 +0200

    Add setup and teardown functions to the main test_suite.c

--- src/tests/test_suite.c
+++ src/tests/test_suite.c
@@ -4,15 +4,33 @@
 // Function declarations to add tests from different modules
 void add_is_valid_string_tests(CU_pSuite suite);
 void add_check_tag_tests(CU_pSuite suite);
+void add_test_mkfile(CU_pSuite suite);
+
+// Setup code
+int global_setup(void) {
+    printf("Setting up the environment\n");
+    return 0;
+}
+
+// Teardown code
+int global_teardown(void) {
+    // Clean up files
+    printf("\nRemoving everything that was created\n");
+    return 0;
+}
 
 int main() {
     // Initialize the CUnit test registry
+    printf("Initializing the CUnit test registry\n");
     if (CUE_SUCCESS != CU_initialize_registry()) {
+        fprintf(stderr, "Failed to initialize the CUnit test registry: %s\n",
+                strerror(errno));
         return CU_get_error();
     }
 
     // Add suite for is_valid_string tests
-    CU_pSuite pSuite1 = CU_add_suite("Suite for is_valid_string", NULL, NULL);
+    printf("Adding suite for is_valid_string\n");
+    CU_pSuite pSuite1 = CU_add_suite("Suite for is_valid_string", global_setup, global_teardown);
     if (NULL == pSuite1) {
         CU_cleanup_registry();
         return CU_get_error();
@@ -20,7 +38,8 @@ int main() {
     add_is_valid_string_tests(pSuite1);
 
     // Add suite for check_tag_name tests
-    CU_pSuite pSuite2 = CU_add_suite("Suite for check_tag", NULL, NULL);
+    printf("Adding suite for check_tag\n");
+    CU_pSuite pSuite2 = CU_add_suite("Suite for check_tag", global_setup, global_teardown);
     if (NULL == pSuite2) {
         CU_cleanup_registry();
         return CU_get_error();
@@ -28,8 +47,10 @@ int main() {
     add_check_tag_tests(pSuite2);
 
     // Run all tests using the CUnit Basic interface
+    printf("Running all tests\n");
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     CU_cleanup_registry();
+    printf("Tests completed\n");
     return CU_get_error();
 }
