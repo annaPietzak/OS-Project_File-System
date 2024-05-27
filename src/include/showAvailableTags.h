3d6220045d79ddd8b4a1356ddde2a54e478cabf2
Anna Pietzak <anna.pietzak@gmail.com>
2024-05-27 21:17:22 +0200

    add method to show all available tags

---
+++ src/include/showAvailableTags.h
@@ -0,0 +1,9 @@
+#ifndef CHECKIFTAGISKNOWN_H
+#define CHECKIFTAGISKNOWN_H
+
+#include <sys/stat.h>
+#include <cstdio>
+
+int show_available_tags(const char * tagName, const char * tagValue);
+
+#endif //CHECKIFTAGISKNOWN_H
---
+++ src/showAvailableTags.c
@@ -0,0 +1,32 @@
+#include "include/showAvailableTags.h"
+
+
+// Check if path points to file and not directory
+int show_available_tags() {
+    FILE * fp;
+    char * line = NULL;
+    char * allTags = "Available tags \n";
+    size_t len = 0;
+    ssize_t read;
+    boolean found = false;
+
+    fp = fopen("ressources/tags.csv", "r");
+    if (fp == NULL)
+        exit(1);
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        allTags = allTags + line + "\n";
+    }
+
+    echo allTags;
+
+    fclose(fp);
+    if (line)
+        free(line);
+    
+    if (found) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
