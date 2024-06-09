592de35780a10cd463f053f64acdd9068b2d7f23
Anna <anna.pietzak@gmail.com>
2024-06-09 17:26:13 +0200

    Fix string comparisson

--- src/checkIfTagIsKnown.c
+++ src/checkIfTagIsKnown.c
@@ -3,24 +3,31 @@
 
 // Check if path points to file and not directory
 int check_if_tag_is_known(const char * tagName, const char * tagValue) {
+    printf("searching tag ");
     FILE * fp;
     char line[1000];
     char inputTag[1000];
     strcpy(inputTag, tagName);
     strcat(inputTag, "; ");
     strcat(inputTag, tagValue);
+    strcat(inputTag, "\n");
+    printf("%s", inputTag);
     bool found = false;
 
-    char pathToTags[] = "/home/";
-    strcat(pathToTags, getenv("USERNAME");
+    char pathToTags[100] = "/home/";
+    strcat(pathToTags, getenv("USERNAME"));
     strcat(pathToTags, "/.eftas/ressources/tags.csv");
+    printf("%s\n",pathToTags);
     fp = fopen(pathToTags, "r");
-    if (fp == NULL)
+//    fp = fopen("/home/anna/.eftas/ressources/tags.csv", "r");
+    if (fp == NULL){
+	printf("error - tags not found\n");
         return 1;
+    }
 
     while (feof(fp) != true){
         fgets(line, 1000, fp);
-        //printf("Tag: %s\n", line);
+        printf("Tag: %s", line);
         if (strcmp(line, inputTag) == 0) {
             found = true;
             break;
@@ -29,10 +36,10 @@ int check_if_tag_is_known(const char * tagName, const char * tagValue) {
 
     fclose(fp);
     if (found) {
-        //printf("Tag exists! \n");
+        printf("Tag exists! \n");
         return 0;
     } else {
-        //printf("Tag does not exists! \n");
+        printf("Tag does not exists! \n");
         return 1;
     }
 }
