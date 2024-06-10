0e41efeb88d94faf7eb1e3dd54c686e825e70de4
Anna Pietzak <anna.pietzak@gmail.com>
2024-06-10 17:32:19 +0200

    add dependendcies to setup file

--- src/setup_eftas.sh
+++ src/setup_eftas.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#install dependency for setfattr and gefattrs (extended attributes)
+sudo apt install attr
+
+#give execution rights to all bashscripts
+chmod +x open_help.sh
+chmod +x remove_eftas.sh
+chmod +x eftas.sh
+chmod +x lsDifferent2.sh
+
 #compile all files with makefile
 make all
 
@@ -17,4 +26,3 @@ mv ../ressources/ -t /home/$USER/.eftas
 
 #move eftas bash script so it can be called like a command
 sudo cp eftas.sh /usr/local/bin/eftas
-
