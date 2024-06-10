58d58766ede3ed57ee7bf9dc798e625dd66553ea
troberts97 <t.roberts@stud.unibas.ch>
2024-06-10 19:48:14 +0200

    Change setup_eftas.sh so it runs with src structure

--- src/setup_eftas.sh
+++ src/setup_eftas.sh
@@ -4,25 +4,27 @@
 sudo apt install attr
 
 #give execution rights to all bashscripts
-chmod +x open_help.sh
-chmod +x remove_eftas.sh
-chmod +x eftas.sh
-chmod +x lsDifferent2.sh
+chmod +x ./open_help.sh
+chmod +x ./remove_eftas.sh
+chmod +x ./eftas.sh
+chmod +x ./lsDifferent2.sh
 
 #compile all files with makefile
 make all
 
-#create hidden eftas folder 
+#create hidden eftas folder
 mkdir /home/$USER/.eftas
 
-#copy all cmopiled files to the directory /home/$USER/.eftas
+#copy all compiled files to the directory /home/$USER/.eftas
 cp ./*.o -t /home/$USER/.eftas
 cp ./*_main -t /home/$USER/.eftas
 cp ./showAvailableTags -t /home/$USER/.eftas
 cp ./*.md -t /home/$USER/.eftas
 cp ./lsDifferent2.sh -t /home/$USER/.eftas
 cp ./open_help.sh -t /home/$USER/.eftas
-cp ../ressources/ -t /home/$USER/.eftas
+cp ../ressources/ -r /home/$USER/.eftas
 
 #move eftas bash script so it can be called like a command
-sudo cp eftas.sh /usr/local/bin/eftas
+sudo cp ./eftas.sh /usr/local/bin/eftas
+
+echo "Successfully compiled all files, eftas is ready for use. Call with 'eftas'."
