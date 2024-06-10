964872984ad3dc70f13b88f3838c36109a1ccd6d
troberts97 <t.roberts@stud.unibas.ch>
2024-06-10 18:22:44 +0200

    Change move files to copy files

--- src/setup_eftas.sh
+++ src/setup_eftas.sh
@@ -15,14 +15,14 @@ make all
 #create hidden eftas folder 
 mkdir /home/$USER/.eftas
 
-#move all cmopiled files to the directory /home/$USER/.eftas
-mv ./*.o -t /home/$USER/.eftas
-mv ./*_main -t /home/$USER/.eftas
-mv ./showAvailableTags -t /home/$USER/.eftas
-mv ./*.md -t /home/$USER/.eftas
-mv ./lsDifferent2.sh -t /home/$USER/.eftas
-mv ./open_help.sh -t /home/$USER/.eftas
-mv ../ressources/ -t /home/$USER/.eftas
+#copy all cmopiled files to the directory /home/$USER/.eftas
+cp ./*.o -t /home/$USER/.eftas
+cp ./*_main -t /home/$USER/.eftas
+cp ./showAvailableTags -t /home/$USER/.eftas
+cp ./*.md -t /home/$USER/.eftas
+cp ./lsDifferent2.sh -t /home/$USER/.eftas
+cp ./open_help.sh -t /home/$USER/.eftas
+cp ../ressources/ -t /home/$USER/.eftas
 
 #move eftas bash script so it can be called like a command
 sudo cp eftas.sh /usr/local/bin/eftas
