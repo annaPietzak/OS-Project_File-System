4e59d7ce3cfc0bd15f4183b33a130ba2c45a8476
troberts97 <t.roberts@stud.unibas.ch>
2024-06-10 18:26:17 +0200

    Change remove_eftas.sh since files only get copied

--- download_eftas/remove_eftas.sh
+++ download_eftas/remove_eftas.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#move all not compiled files back
-mv /home/$USER/.eftas/*.md -t ./src/
-mv /home/$USER/.eftas/lsDifferent2.sh -t ./src/
-mv /home/$USER/.eftas/open_help.sh -t ./src/
-mv /home/$USER/.eftas/ressources/ -t .
-
 #remove hidden eftas folder
 rm -dr /home/$USER/.eftas
 
