727e201c6d2bc01cfa11a5c0382173d847547238
troberts97 <t.roberts@stud.unibas.ch>
2024-06-10 19:11:43 +0200

    Revert "setup and remove were still old version"
    This reverts commit b731bd675168aae885d456ca3c7a5bda2bca2dcc.

--- src/remove_eftas.sh
+++ src/remove_eftas.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+#move all not compiled files back
+mv /home/$USER/.eftas/*.md -t ./
+mv /home/$USER/.eftas/lsDifferent2.sh -t ./
+mv /home/$USER/.eftas/open_help.sh -t ./
+mv /home/$USER/.eftas/ressources/ -t ..
+
 #remove hidden eftas folder
 rm -dr /home/$USER/.eftas
 
 #remove eftas bash script again
 sudo rm /usr/local/bin/eftas
-
-echo "Removed all compiled files, restored download folder to origin."
--- src/setup_eftas.sh
+++ src/setup_eftas.sh
@@ -4,29 +4,25 @@
 sudo apt install attr
 
 #give execution rights to all bashscripts
-chmod +x ./src/open_help.sh
-chmod +x ./remove_eftas.sh
-chmod +x ./src/eftas.sh
-chmod +x ./src/lsDifferent2.sh
+chmod +x open_help.sh
+chmod +x remove_eftas.sh
+chmod +x eftas.sh
+chmod +x lsDifferent2.sh
 
 #compile all files with makefile
-cd ./src
 make all
-cd ..
 
 #create hidden eftas folder 
 mkdir /home/$USER/.eftas
 
-#move all cmopiled files to the directory /home/$USER/.eftas
-mv ./src/*.o -t /home/$USER/.eftas
-mv ./src/*_main -t /home/$USER/.eftas
-mv ./src/showAvailableTags -t /home/$USER/.eftas
-mv ./src/*.md -t /home/$USER/.eftas
-mv ./src/lsDifferent2.sh -t /home/$USER/.eftas
-mv ./src/open_help.sh -t /home/$USER/.eftas
-mv ./ressources/ -t /home/$USER/.eftas
+#copy all cmopiled files to the directory /home/$USER/.eftas
+cp ./*.o -t /home/$USER/.eftas
+cp ./*_main -t /home/$USER/.eftas
+cp ./showAvailableTags -t /home/$USER/.eftas
+cp ./*.md -t /home/$USER/.eftas
+cp ./lsDifferent2.sh -t /home/$USER/.eftas
+cp ./open_help.sh -t /home/$USER/.eftas
+cp ../ressources/ -t /home/$USER/.eftas
 
 #move eftas bash script so it can be called like a command
-sudo cp ./src/eftas.sh /usr/local/bin/eftas
-
-echo "Successfully compiled all files, eftas is ready for use. Call with 'eftas'."
+sudo cp eftas.sh /usr/local/bin/eftas
