7154d6cc13dcc868ae3c6e32ca89daeeae15c851
troberts97 <t.roberts@stud.unibas.ch>
2024-06-11 12:47:02 +0200

    Add make clean

--- src/remove_eftas.sh
+++ src/remove_eftas.sh
@@ -6,4 +6,7 @@ rm -dr /home/$USER/.eftas
 #remove eftas bash script again
 sudo rm /usr/local/bin/eftas
 
+#remove the created files again
+make clean
+
 echo "Removed all compiled files, restored download folder to origin."
