89d28ac1f423c5ece4121d31600f7baca345fc3e
troberts97 <t.roberts@stud.unibas.ch>
2024-06-11 17:32:32 +0200

    Fix some issues when using it in a freshly set up environment

--- download_eftas/setup_eftas.sh
+++ download_eftas/setup_eftas.sh
@@ -27,10 +27,11 @@ cp ./src/showAvailableTags -t /home/$USER/.eftas
 cp ./src/*.md -t /home/$USER/.eftas
 cp ./src/lsDifferent2.sh -t /home/$USER/.eftas
 cp ./src/open_help.sh -t /home/$USER/.eftas
-cp ./src/ressources/ -t /home/$USER/.eftas
+cp ./src/ressources/ -r /home/$USER/.eftas
 
 # Move eftas bash script so it can be called like a regular command
 sudo cp ./src/eftas.sh /usr/local/bin/eftas
+sudo chmod 755 /usr/local/bin/eftas
 
 echo "Successfully compiled all files, eftas is ready for use now."
 echo "Call our extension with 'eftas' to get started."
